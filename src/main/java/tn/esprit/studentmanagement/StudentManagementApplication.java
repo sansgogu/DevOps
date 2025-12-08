db98945998311dd570b6790fa6ed0518b48f14ed
aymenbenabdallah <150945354+sansgogu@users.noreply.github.com>
2025-12-08 11:17:00 +0100

    Update StudentManagementApplication.java

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,13 +1,15 @@
 package tn.esprit.studentmanagement;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootApplication
-public class StudentManagementApplication {
+@SpringBootTest
+@ActiveProfiles("test")   // 👉 très important : sur la classe
+class StudentManagementApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StudentManagementApplication.class, args);
+    @Test
+    void contextLoads() {
+        // test vide : on vérifie juste que le contexte Spring démarre
     }
-
 }

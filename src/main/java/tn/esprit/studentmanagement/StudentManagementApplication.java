c9d467c57e1bd8909b4492576a5ffb06bc78e2a7
aymenbenabdallah <150945354+sansgogu@users.noreply.github.com>
2025-12-09 09:47:54 +0100

    Delete StudentManagementApplicationTests class
    Removed test class for StudentManagementApplication.

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,15 +1,1 @@
-package tn.esprit.studentmanagement;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")   // 👉 très important : sur la classe
-class StudentManagementApplicationTests {
-
-    @Test
-    void contextLoads() {
-        // test vide : on vérifie juste que le contexte Spring démarre
-    }
-}
